03211042c4bbbc2711b37b3e67c2c9038d152f3d
Cristina Voivod <cristinavoivod@yahoo.com>
2025-11-05 18:55:41 +0200

    *

--- src/main/java/com/example/mall_management/MallManagementApplication.java
+++ src/main/java/com/example/mall_management/MallManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+//*
 @SpringBootApplication(scanBasePackages = "com.example.mall_management")
 public class MallManagementApplication {
 
